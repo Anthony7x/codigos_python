5b15e10138d672142969ad86a758b721b004217d
Anthony Bruno <anthonyqy333@gmail.com>
2024-09-03 15:05:08 -0300

    Trabalhando com dados de entrada e saida em python

---
+++ exemplo2.py
@@ -0,0 +1,13 @@
+nome = "Anthony Bruno"
+end = "Maiobão"
+idade = 18
+
+# 1ª forma de concateneção
+print("Olá ", nome," seu endereço é: ", end," sua idade é: ", idade)
+
+# 2ª forma de concatenação
+print("Seja  Bem-vindo {} sua residência está na {} e você possui {}".format(nome,end,idade))
+
+# 3ª forma de concatenação - atual e mais ultilizada
+print(f"Olá, Seja Bem-vindo {nome}, e o seu endereço é {end} e a sua idade é {idade}")
+
