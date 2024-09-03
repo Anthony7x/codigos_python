1739da7201163429606a0a092a62143d2f9f710a
Anthony Bruno <anthonyqy333@gmail.com>
2024-09-03 16:51:02 -0300

    Exercicio referente a python

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+idade_anos = int(input("Digite sua idade: "))
+
+idadeMeses = idade_anos * 12
+idadeAnos = idade_anos * 365
+
+print(f"Sua idade em meses é: {idadeMeses}.")
+print(f"Sua idade em anos é: {idadeAnos}")
---
+++ exercicio2.py
@@ -0,0 +1,7 @@
+hora = int(input("Digite o tempo de duração: "))
+
+horaMin = hora * 60
+horaSec = hora * 3600
+
+print(f"Digite a hora para converter em minutos: {horaMin}")
+print(f"Digite a hora para converter em segundos: {horaSec}")
