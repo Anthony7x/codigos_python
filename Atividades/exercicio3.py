a83caf9aec58c2054222b72917e1ee9ab1305bc5
Anthony Bruno <anthonyqy333@gmail.com>
2024-09-03 17:06:02 -0300

    Exercicio da Fazenda

---
+++ Atividades/exercicio3.py
@@ -0,0 +1,6 @@
+nomeFazenda = input("Digite o nome da sua fazenda: ")
+qtdCavalo = int(input("Digite a quantidade de cavalos: "))
+
+ferradura = qtdCavalo * 4
+
+print(f"A fazenda {nomeFazenda} possui {qtdCavalo} cavalos, a quantidade total de ferradura para equipar todos os cavalos é: {ferradura}.")
